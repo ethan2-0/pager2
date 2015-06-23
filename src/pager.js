8798d09787692e4e443d019bbae720b593d35b45
ethan2-0 <me@example.com>
2015-06-23 16:54:20 -0400

    BSD license and more comments in pager.js and tests.js

--- src/pager.js
+++ src/pager.js
@@ -1,3 +1,38 @@
+// Copyright (c) 2015 Ethan White.
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
+// conditions are met:
+// 
+// 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the distribution.
+// 
+// 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived
+// from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+// NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+// THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/*
+
+Some things to note:
+ * When we set the property "oldDisplay" on a node, what that's doing is
+   caching what its display was before it got hidden, so that we can restore
+   that when we make it visible again.
+ * Pager.initialized is the complement of the dirty bit on the page cache.
+   What that means is it's the opposite of whether the page cache needs to be
+   regenerated; that is, when Pager.initialized === false, the page cache must
+   be regenerated (or generated, if it's the first time).
+ * The page cache is the cache of the pages that have been found. I didn't want
+   to thrash the DOM every time a page gets loaded. I probably could have also
+   done it with a custom CSS selector, but I *think* that this is faster.
+
+*/
 function Pager(pageName, option) {
     if(option == "hashTrumpsName") {
         if(window.location.hash != "" && window.location.hash != "#") {
@@ -16,20 +51,23 @@ function Pager(pageName, option) {
         }
         Pager.initialized = true;
     }
+    // Did we find the page we were looking for? If not, print out a warning later.
     var foundIt = false;
+    // Loop through all of the pages. Hide all of the pages we don't want to see
+    // (i.e. those whose name != pageName), and show the page we want to see.
     for(var i = 0; i < Pager.pageKeys.length; i++) {
         var key = Pager.pageKeys[i];
         var page = Pager.pageCache[key];
         if(key == pageName) {
             foundIt = true;
-            if(typeof page.oldDisplay === "undefined") {
+            if(typeof page.oldDisplay === "undefined") { // If page.oldDisplay is false, then this is the first time through, so remember it.
                 // Do nothing, since this is the first time through; but, cache it.
                 page.oldDisplay = page.style.display;
-            } else {
+            } else { // Otherwise, just set it back.
                 page.style.display = page.oldDisplay;
             }
         } else {
-            if(page.style.display !== "none") {
+            if(page.style.display !== "none") { // If the page isn't already hidden, we want to update (or or remember for the first time) oldDisplay.
                 page.oldDisplay = page.style.display;
             }
             page.style.display = "none";
